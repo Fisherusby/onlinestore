e90efb4f6b2a4de1a440937b7edb289397fcd3b8
Fisher <fisheriusby@gmail.com>
2023-03-22 16:07:59 +0300

    rebuild migration, add static

---
+++ onlinestore/settings_local.py
@@ -0,0 +1,14 @@
+
+
+SECRET_KEY = 'django-insecure-*!+(xv%l(d^-&flr4pfg#q3$@204o53m0+scca3j_onq*2wb_9'
+DEBUG = True
+
+
+
+EMAIL_HOST_USER = 'notify.fisher.dev@gmail.com'
+EMAIL_HOST_PASSWORD = 'tfjvmklhckoxzelq'
+
+BRAINTREE_MERCHANT_ID = '8qrhvnyqny9s4kqs' # Merchant ID
+BRAINTREE_PUBLIC_KEY = 'tyr229qg67gt2pty' # Public Key
+BRAINTREE_PRIVATE_KEY = '5f097ac72115b6c8c32f75334cf42021' # Private key
+
